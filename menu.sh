957c739656e215dbe43f6100929a20bd7631b2d3
Miguel Antonio Chamorro <42693777+Xamorro95@users.noreply.github.com>
2018-08-27 14:20:50 +0200

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,20 +1,60 @@
 #!/bin/bash
-# Declaro valor de variable $bucle para generar un bucle con control
-bucle=1
+#
+# Genera un menu con diferentes opciones para la creacion y gestion de un proyecto
+# Autor: Miguel Chamorro
+#
 
-while [[ $bucle == 1 ]];
-do	
-	echo "--------------MENU PRINCIPAL-----------"
-	echo "1. Crear proyecto"
-	echo "0. Exit"
-	read -p "Que deseas hacer: " opcion
-	if [[ $opcion == 1 ]];
-	then	
-		source crearproyecto.sh
-		elif [[ $opcion == 0 ]];
-		then
-			bucle=0
+#Evita que se comprueben los ejemplos de otros scripts
+EJECUTAR_EJEMPLOS_UTILIDADES=false
+EJECUTAR_EJEMPLOS_USUARIOS=false
+EJECUTAR_EJEMPLOS_CREACION_PROYECTO=false
+
+# 
+# Constantes utilizadas en las funciones
+#
+source formato.sh
+source utilidades.sh
+source crearproyecto.sh
+
+#Compruebo si estoy  dentro de un proyecto comprobando si existe el fichero proyecto.propierties
+
+while [[ true ]];
+do
+	clear
+	echo_titulo "MENU PRINCIPAL"
+	if [[ -e proyecto.properties ]]; then
+		echo " 1. Compilar"
+		echo " 2. Probar"
+		echo " 3. Backup src"
+		echo " 4. Backup bin"
+		echo " 5. Borrar bin"
 	else
-		echo "Ese parametro no esta contemplado"
+		echo " 6. Crear proyecto"
 	fi
+	echo
+	echo " 0. Salir"
+	echo
+	PreguntarValor opcion_seleccionada "Que quieres hacer?"
+	echo 
+
+	case $opcion_seleccionada in
+		0)
+			break
+			;; 
+		6) 
+			if [[ -e proyecto.properties ]]; then			
+				echo_error "Por favor, pulsa una opcion valida!"
+				read -p "Pulsa cualquier tecla para continuar..." -N 1 
+			else
+				CrearProyecto
+			fi
+			;;
+		*)
+			echo_error "Por favor, pulsa una opcion valida!"
+			read -p "Pulsa cualquier tecla para continuar..." -N 1 
+			;;
+	esac
+			
 done
+
+clear
