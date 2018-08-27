34ce70e1caef672f61e015aecb53fc7aea42b940
Miguel Antonio Chamorro <42693777+Xamorro95@users.noreply.github.com>
2018-08-27 17:40:32 +0200

    Create compilar.sh

---
+++ compilar.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Funciones para compilacion
+# Autor: Miguel Chamorro
+#
+
+# 
+# Constantes utilizadas en las funciones
+#
+
+
+# Utilidades para preguntar por consola al usuario preguntas
+source utilidades.sh
+source utilidadesUsuarios.sh
+
+function Compilar(){
+	echo "Compilando...."
+	echo_pausa
+}
