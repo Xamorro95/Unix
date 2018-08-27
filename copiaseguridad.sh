a3e07fa2cb5f181e0ae4aadf4b40d176e6c4eaa7
Miguel Antonio Chamorro <42693777+Xamorro95@users.noreply.github.com>
2018-08-27 17:18:28 +0200

    Update copiaseguridad.sh

--- copiaseguridad.sh
+++ copiaseguridad.sh
@@ -9,7 +9,9 @@
 #
 source proyecto.properties
 
-
+#
+#Recibe por parametro la carpeta sobre la cual hay que realizar la copia
+#
 function CopiaSeguridad(){
 	#Almaceno en una variable la fecha, hora y minutos del momento de realizar la copia de seguridad
 	fecha=$(date "+%Y%m%d_%H%M")
@@ -17,22 +19,22 @@ function CopiaSeguridad(){
 	#Creamos la ruta de carpetas necesarias para almacenar la copia
 	mkdir temp &> /dev/null
 	mkdir temp/backups &> /dev/null
-	mkdir temp/backups/src &> /dev/null
-	mkdir temp/backups/src/$fecha &> /dev/null
+	mkdir temp/backups/$1 &> /dev/null
+	mkdir temp/backups/$1/$fecha &> /dev/null
 
 	#Comprimimos la copia de seguridad
-	tar -cvf ./temp/backups/src/$fecha/$PROYECTO.tar.gz ./src
+	tar -cvf ./temp/backups/$1/$fecha/$PROYECTO.tar.gz ./src
 	
 	#Cambiamos permisos y propietario al tar.gz creado
-	sudo chmod -R 604 ./temp/backups/src/$fecha
-	sudo chown -R backupP ./temp/backups/src/$fecha
+	sudo chmod -R 604 ./temp/backups/$1/$fecha
+	sudo chown -R backupP ./temp/backups/$1/$fecha
 	
 	#Copiamos el tar.gz a backup
-	sudo mv ./temp/backups/src/$fecha ./backup/src
+	sudo mv ./temp/backups/$1/$fecha ./backup/$1
 	
 	#Borramos la carpeta temporal creada
 	sudo rm -R ./temp
 
-	echo "Creada copia de seguridad src"
+	echo "Creada copia de seguridad $1"
 	echo_pausa
 }
