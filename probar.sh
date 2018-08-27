e54ed1e40057b79574ca264021cdcb99fdd567d2
Miguel Antonio Chamorro <42693777+Xamorro95@users.noreply.github.com>
2018-08-27 17:40:13 +0200

    Create probar.sh

---
+++ probar.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Funciones para probar
+# Autor: Miguel Chamorro
+#
+
+# 
+# Constantes utilizadas en las funciones
+#
+
+
+# Utilidades para preguntar por consola al usuario preguntas
+source utilidades.sh
+source utilidadesUsuarios.sh
+
+function probar(){
+	echo "probando...."
+	echo_pausa
+}
